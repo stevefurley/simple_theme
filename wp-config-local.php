4fe52384d579d2d2e7ca923884c40e8ebcb448db
stevesnow100 <stevefurley@gmail.com>
2016-11-09 23:57:59 +0000

    updating plugins etc

---
+++ wp-config-local.php
@@ -0,0 +1,28 @@
+<?php
+// ===================================================
+// Local Env DB Config
+// ===================================================
+
+/** The name of the database for WordPress */
+define('DB_NAME', 'legal');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+define('WP_ENV', "local");
+
+define('WP_DEBUG', true);
+
+define('SAVEQUERIES', true);
--- wp-config.php
+++ wp-config.php
@@ -24,13 +24,13 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 if ($local_env == true):
-define('DB_NAME', 'simple_theme');
+define('DB_NAME', 'legal');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'pass4burfield');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
